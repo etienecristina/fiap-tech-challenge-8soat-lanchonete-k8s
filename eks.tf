0c1da223e761665a0b1c3b6a8c77478c61450250
etienecristina <etiene.scristina@gmail.com>
2025-01-27 20:53:32 -0300

    Alterando roles

--- eks.tf
+++ eks.tf
@@ -140,7 +140,8 @@ resource "aws_iam_role_policy_attachment" "vpc_cni_policy" {
 resource "aws_eks_node_group" "fiap_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "${var.cluster_name}-ng"
-  node_role_arn   = aws_iam_role.eks_role.arn
+  node_role_arn = aws_iam_role.node_group_role.arn
+
   subnet_ids      = aws_subnet.private_subnet[*].id
   disk_size       = 50
   instance_types  = ["t3a.medium"]
@@ -155,6 +156,39 @@ resource "aws_eks_node_group" "fiap_node_group" {
     max_unavailable = 1
   }
 }
+
+resource "aws_iam_role" "node_group_role" {
+  name = "${var.cluster_name}-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ec2_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 
 # Outputs
 output "eks_cluster_endpoint" {
