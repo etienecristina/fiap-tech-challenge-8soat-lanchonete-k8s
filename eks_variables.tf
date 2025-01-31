434e0f704ec093f94b0cbe39e9f70a36d302fa78
etienecristina <etiene.scristina@gmail.com>
2025-01-30 23:23:13 -0300

    reestruturação

---
+++ eks_variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
