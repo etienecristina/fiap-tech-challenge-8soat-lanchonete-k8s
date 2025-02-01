d8124b1da81e2195fc0e782cfaa6cb10c9d00917
etienecristina <etiene.scristina@gmail.com>
2025-02-01 17:22:02 -0300

    Implementation of the terraform

---
+++ 20 - eks.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet_private_1.id, aws_subnet.subnet_private_2.id]
+    endpoint_public_access = false
+    endpoint_private_access = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
