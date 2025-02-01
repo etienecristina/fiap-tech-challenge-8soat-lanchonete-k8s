dfc0fdf7ee79d6120d05878f7a3a7e7c2c9d6b68
etienecristina <etiene.scristina@gmail.com>
2025-02-01 20:23:26 -0300

    Fix policies

--- 25 - worker-nodes.tf
+++ 25 - worker-nodes.tf
@@ -15,6 +15,41 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
+resource "aws_iam_role_policy" "eks_network_policy" {
+  name = "eks-network-policy"
+  role = aws_iam_role.eks_node_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ec2:UnassignPrivateIpAddresses",
+          "ec2:ModifyNetworkInterfaceAttribute",
+          "ec2:DetachNetworkInterface",
+          "ec2:DescribeTags",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DeleteNetworkInterface",
+          "ec2:CreateNetworkInterface",
+          "ec2:AttachNetworkInterface",
+          "ec2:AssignPrivateIpAddresses"
+        ],
+        "Resource": "*",
+        "Sid": "IPV4"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "ec2:CreateTags",
+        "Resource": "arn:aws:ec2:*:*:network-interface/*",
+        "Sid": "CreateTags"
+      }
+    ]
+  })
+}
+
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name = aws_eks_cluster.eks.name
   node_group_name = "my-node-group"
