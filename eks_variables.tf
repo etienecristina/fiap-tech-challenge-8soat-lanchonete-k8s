b79ff16f6f5d383be7530e81644350baaae5acbc
etienecristina <etiene.scristina@gmail.com>
2025-02-01 16:27:54 -0300

    Alterando arquivo de variáveis

--- eks_variables.tf
+++ eks_variables.tf
@@ -12,4 +12,5 @@ variable "vpc_id" {
 variable "subnet_ids" {
   description = "List of subnet IDs"
   type        = list(string)
+  default = ["subnet-0b3e21dff62bc9fa2", "subnet-0c1a4e750736c39e7"]
 }
